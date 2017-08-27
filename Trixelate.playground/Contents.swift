7b5a6585b68eb15786cb6e8b25a122bf620ebeb5
Rob Stearn <rob@Kino.lan>
2017-08-27 15:38:11 +0100

    serious progress

--- Trixelate.playground/Contents.swift
+++ Trixelate.playground/Contents.swift
@@ -22,51 +22,55 @@ class TrixelatedView: UIView {
 		self.backgroundColor = UIColor.white
 
 		let ratio = self.frame.size.width / self.frame.size.height
-		let trixelHeight = Float(self.frame.size.height / 7)
-		let trixelWidth = Float(trixelHeight) * Float(ratio)
-    let columns = Float(self.frame.size.width) / trixelWidth
-    let rows = Float(self.frame.size.height) / trixelHeight
+		let trixelHeight = self.frame.size.height / 30
+		let trixelWidth = trixelHeight * ratio
+    let columns = self.frame.size.width / trixelWidth
+    let rows = self.frame.size.height / trixelHeight
 
 		let context = UIGraphicsGetCurrentContext()
 		
     for column in 0 ..< Int(columns) {
-      let baseX = Float(column) * Float(trixelWidth)
-      
-      let offset = Float(column).truncatingRemainder(dividingBy: Float(2.0)) == 0 ? Float(0.5) : Float(1.0)
-      
-      //Float(column) % 2 > 0 ? Float(0.5) : Float(1) //Float(modf(Float(column)).0 > 0 ? 0.5 : 1)
+      let baseX = CGFloat(column) * trixelWidth
+    
       for row in 0 ..< Int(rows) {
-        let baseY = Float(row) * Float(trixelHeight)
+        let offset = CGFloat(column).truncatingRemainder(dividingBy: 2.0) == 0 ? 0.0 : -0.5 as CGFloat
+        let baseY = CGFloat(row) * trixelHeight + (trixelHeight * offset)
         let bezierPath = UIBezierPath()
-        bezierPath.move(to: CGPoint(x: CGFloat(baseX), y: CGFloat(baseY)))
-        bezierPath.addLine(to: CGPoint(x: CGFloat(baseX + trixelWidth),
-                                       y: CGFloat(baseY - (trixelHeight * offset))))
-        bezierPath.addLine(to: CGPoint(x: CGFloat(baseX + trixelWidth),
-                                       y: CGFloat(baseY + (trixelHeight * offset))))
+        bezierPath.lineWidth = 0.0
+        bezierPath.move(to: CGPoint(x: baseX, y: baseY ))
+        bezierPath.addLine(to: CGPoint(x: baseX + trixelWidth,
+                                       y: (baseY - (trixelHeight * 0.5))))
+        bezierPath.addLine(to: CGPoint(x: baseX + trixelWidth,
+                                       y: (baseY + (trixelHeight * 0.5))))
         bezierPath.close()
         bezierPath.stroke()
-        let vector = CIVector(cgRect: CGRect(x: CGFloat(baseX + trixelWidth * offset),
-                                             y: CGFloat(baseY),
-                                             width: CGFloat(trixelWidth),
-                                             height: CGFloat(trixelHeight)))
-        let averageColour = self.image?.areaAverage(in: vector)
-        context?.setFillColor((averageColour?.cgColor)!)
+        let fromRect = CGRect(x: CGFloat(baseX + trixelWidth * 0.5),
+                            y: CGFloat(baseY),
+                            width: CGFloat(trixelWidth),
+                            height: CGFloat(trixelHeight))
+        let drawImage = self.image?.cgImage!.cropping(to: fromRect)
+        let bimage = AveragableImage(cgImage: drawImage!)
+        let averageColour = bimage.averageColor().cgColor //areaAverage(in: vector).cgColor //= UIColor.blue.cgColor//
+        context?.setFillColor(averageColour)
         bezierPath.fill()
         
         let bezierPathB = UIBezierPath()
-        bezierPathB.move(to: CGPoint(x: CGFloat(baseX), y: CGFloat(baseY)))
-        bezierPathB.addLine(to: CGPoint(x: CGFloat(baseX),
-                                       y: CGFloat(baseY + trixelHeight)))
-        bezierPathB.addLine(to: CGPoint(x: CGFloat(baseX + trixelWidth),
-                                       y: CGFloat(baseY + (trixelHeight * offset))))
+        bezierPathB.lineWidth = 0.0
+        bezierPathB.move(to: CGPoint(x: baseX, y: baseY))
+        bezierPathB.addLine(to: CGPoint(x: baseX + trixelWidth,
+                                        y: (baseY + trixelHeight * 0.5)))
+        bezierPathB.addLine(to: CGPoint(x: baseX,
+                                        y: (baseY + trixelHeight)))
         bezierPathB.close()
         bezierPathB.stroke()
-        let vectorB = CIVector(cgRect: CGRect(x: CGFloat(baseX + trixelWidth * offset),
-                                             y: CGFloat(baseY),
-                                             width: CGFloat(trixelWidth),
-                                             height: CGFloat(trixelHeight)))
-        let averageColourB = self.image?.areaAverage(in: vectorB)
-        context?.setFillColor((averageColourB?.cgColor)!)
+        let fromRectB = CGRect(x: CGFloat(baseX + trixelWidth * 0.5),
+                             y: CGFloat(baseY),
+                             width: CGFloat(trixelWidth),
+                             height: CGFloat(trixelHeight))
+        let drawImageB = self.image?.cgImage!.cropping(to: fromRectB)
+        let cimage = AveragableImage(cgImage: drawImageB!)
+        let averageColourB = cimage.averageColor().cgColor //areaAverage(in: vector).cgColor //= UIColor.blue.cgColor//
+        context?.setFillColor(averageColourB)
         bezierPathB.fill()
       }
     }
@@ -75,28 +79,38 @@ class TrixelatedView: UIView {
 
 class AveragableImage: UIImage {
   
-	func areaAverage(in vector: CIVector) -> UIColor {
-		var bitmap = [UInt8](repeating: 0, count: 4)
-		// Get average color.
-		let context = CIContext()
-		let inputImage: CIImage = ciImage ?? CoreImage.CIImage(cgImage: cgImage!)
-		let inputExtent = vector
-		let filter = CIFilter(name: "CIAreaAverage", withInputParameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: inputExtent])!
-		let outputImage = filter.outputImage!
-		let outputExtent = outputImage.extent
-		assert(outputExtent.size.width == 1 && outputExtent.size.height == 1)
-		// Render to bitmap.
-		context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: CGColorSpaceCreateDeviceRGB())
-		// Compute result.
-		let result = UIColor(red: CGFloat(bitmap[0]) / 255.0, green: CGFloat(bitmap[1]) / 255.0, blue: CGFloat(bitmap[2]) / 255.0, alpha: CGFloat(bitmap[3]) / 255.0)
-		return result
-	}
+  convenience init(view: UIView) {
+    UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0)
+    view.drawHierarchy(in: view.bounds, afterScreenUpdates: false)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    self.init(cgImage: (image?.cgImage)!)
+  }
+  
+  func averageColor() -> UIColor {
+    let rgba = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 4)
+    let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+    let info = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+    let context: CGContext = CGContext(data: rgba, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: info.rawValue)!
+    context.draw(self.cgImage!, in: CGRect(x:0, y:0, width:1, height:1))
+    
+    if rgba[3] > 0 {
+      let alpha: CGFloat = CGFloat(rgba[3]) / 255.0
+      let multiplier: CGFloat = alpha / 255.0
+      return UIColor(red: CGFloat(rgba[0]) * multiplier, green: CGFloat(rgba[1]) * multiplier, blue: CGFloat(rgba[2]) * multiplier, alpha: alpha)
+    } else {
+      return UIColor(red: CGFloat(rgba[0]) / 255.0, green: CGFloat(rgba[1]) / 255.0, blue: CGFloat(rgba[2]) / 255.0, alpha: CGFloat(rgba[3]) / 255.0)
+    }
+  }
+  
 }
 
-guard let fileUrl = Bundle.main.url(forResource: "IMG_3730", withExtension: "JPG") else { fatalError() }
+
+guard let fileUrl = Bundle.main.url(forResource: "MRS_6459schukar2048", withExtension: "jpg") else { fatalError() }
 let data = try Data(contentsOf: fileUrl)
 let image = AveragableImage(data: data)
 let trixelTest = TrixelatedView(image: image!)
+let trixellatedImage = AveragableImage(view: trixelTest)
 
 
 
